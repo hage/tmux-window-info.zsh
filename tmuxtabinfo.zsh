422f316259b02f923c068104cfac1ca3ba53154d
URA Takefumi <ura.takefumi@gmail.com>
2019-06-26 02:27:28 +0900

    [feat] 本体を作った

---
+++ tmuxtabinfo.zsh
@@ -0,0 +1,21 @@
+function is_tmux_runnning() { [ ! -z "$TMUX" ]; }
+
+function tmuxtabinfo() {
+    local tabinfo
+    if is_tmux_runnning ; then
+        project_name=`tmuxtabinfo_projectname`
+        tabinfo=`basename $PWD`
+        tmux rename-window "$project_name//$tabinfo"
+    fi
+}
+
+function tmuxtabinfo_projectname {} {
+    local project_name=`git rev-parse --show-toplevel 2>/dev/null`
+    project_name=`git rev-parse --show-toplevel 2>/dev/null`
+    if [ ! $project_name ]; then
+        project_name=.
+    fi
+    echo `basename $project_name`
+}
+autoload -Uz add-zsh-hook
+add-zsh-hook chpwd tmuxtabinfo
